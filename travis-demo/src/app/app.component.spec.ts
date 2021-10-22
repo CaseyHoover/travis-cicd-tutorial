4699dad620d16c49f16ed76afc9862ca855734e9
Casey Hoover <cwhoover@vt.edu>
2021-10-22 01:55:33 -0400

    fail test on purpose

--- travis-demo/src/app/app.component.spec.ts
+++ travis-demo/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'travis-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('travis-demo');
+    expect(app.title).toEqual('travi-demo');
   });
 
   it('should render title', () => {
